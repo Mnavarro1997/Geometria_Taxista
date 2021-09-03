6c5d3c9b3a31890dbe46780bf0e26187834ad0b4
MNavarro1997 <mnavarro@gteam.es>
2021-09-03 13:45:45 +0200

    Agregar archivos de proyecto.

---
+++ Geometria_Taxista/Interfaces/IPointable.cs
@@ -0,0 +1,13 @@
+﻿
+using Geometria_Taxista.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometria_Taxista.Interfaces
+{
+    public interface IPointable
+    {
+        public int ManhattanDistance(Pointed point);
+    }
+}
---
+++ Geometria_Taxista/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Geometria_Taxista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coord1;
+            int coord2;
+            Console.WriteLine("Write de first one");
+            coord1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write de second one");
+             coord2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
